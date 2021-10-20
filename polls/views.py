2e6e8b6fbe2a490297a883a08386568a46972bf6
Aniket Mishra <ianiket23@gmail.com>
2021-10-20 17:36:18 -0600

    update

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Question
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from .models import Choice, Question
-
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
 
-from .models import Choice, Question
-
+from .models import Question, Choice
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
@@ -23,16 +13,13 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
-
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
-
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
@@ -50,4 +37,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('results', args=(question.id,))) # 'polls:results'
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) # 'polls:results'
